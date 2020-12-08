2e73f4fd0b2e698c337a148aa57898644f30744c
[Sharon005] <[sharonts123@gmail.com]>
2020-12-08 12:56:22 +0530

    Sharon-Contact

--- src/components/SRContact/SRContact.jsx
+++ src/components/SRContact/SRContact.jsx
@@ -6,13 +6,12 @@ import Logo from "../../assets/img/contact.png"
 
 function SRContact() {
   return (
-   <srcontact id="srcontact">
+     <div className="container">
      <Row>
-       <marquee behavior="scroll" direction="left" scrollamount="20"><h3>ROBOCON 20-21 Welcome to CotactUs page. Help us to know better. Thanks for the feedback!!</h3></marquee>
        <Col span={12}>
-       <div>
-         <h1>Welcome to ContactUs page</h1> 
-         <h2>Help us to know better. &#128512;</h2> <br />         
+       <div className="intro">
+         <h1 className="h1">Welcome to ContactUs page</h1> 
+         <h2 className="h2">Help us to know better. &#128512;</h2> <br />         
          <div className="forms">
            <Forms/>
          </div>
@@ -24,8 +23,9 @@ function SRContact() {
           <img className="img-fluid-animated"  src={Logo} alt="logo"  />
         </div>
        </Col>     
-     </Row>    
-   </srcontact>
+     </Row> 
+     </div>   
+   
       
           
     
